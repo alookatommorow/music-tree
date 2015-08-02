c7ddb503f6d18ad4a0da3089c765c1eb0a21ff79
alookatommorow <hessjohnt@gmail.com>
2015-08-01 22:27:06 -0700

    add js file

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,8 @@
 class HomeController < ApplicationController
 
     def index
-        # @artist = ENV["CONSUMER_SECRET"]
-        p @artist = Discogs::Client.new.artist
-        p @results = Discogs::Client.new.search("steve")
+        @artist = Discogs::Client.new.artist
+        @results = Discogs::Client.new.search("steve")
     end
 
 end
